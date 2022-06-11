cad58f1dcc7fd941c0e54e59e4803623527e7932
hyooyh8910 <hyooyh8910@naver.com>
2022-06-11 17:42:49 +0900

    (view) Login page

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,5 +1,133 @@
-const Login = () => {
-  return <div>Login 페이지입니다</div>;
+
+import React from "react";
+// import "./Login.css"
+
+
+
+import styled from "styled-components";
+function Login() {
+
+
+  return (
+    <MyDiv>
+      <MyH1>Show me your space</MyH1>
+      <form>
+        <MyUl>
+          <MyLi>
+            {/* <MyLabel htmlFor="user_id">아이디:</MyLabel> */}
+            <hr/>
+            <InputDiv>
+              <MyInput
+                id="user_id"
+                // value={id}
+                // onChange={onChangeId}
+                placeholder="e-mail을 입력해주세요"
+                required
+              />
+             
+            </InputDiv>
+          </MyLi>
+          
+          <MyLi>
+            {/* <MyLabel htmlFor="user_pwd">비밀번호:</MyLabel> */}
+            <InputDiv>
+              <MyInput
+                id="user_pwd"
+                // value={pwd}
+                // onChange={onChangePwd}
+                placeholder="비밀번호를 입력해주세요"
+                required
+              />
+              
+            </InputDiv>
+          </MyLi>
+        </MyUl>
+        <SubmitInput type="submit" value="로그인" />
+      </form>
+    </MyDiv>
+  );
 };
+
+
+
+
+export const MyDiv = styled.div`
+  padding: 0px;
+  text-align: center;
+  height: 500px;
+  width: 450px; 
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+
+  margin: 100px auto;
+  border: 1px solid #FDDA68;
+  border-radius: 10px;
+ 
+
+`;
+export const MyH1 = styled.h1`
+  font-family: "Kdam Thmor Pro", sans-serif;
+  color: #8873F0;
+  font-size: 35px;
+  padding: 30px;
+  margin-top: -20px;
+`;
+export const MyUl = styled.ul`
+  list-style: none;
+  padding: 50px;
+`;
+export const MyLi = styled.li`
+  width: 400px;
+  display: flex;
+  align-items: center;
+  font-size: 18px;
+  margin:0 auto;
+`;
+export const InputDiv = styled.div`
+  width: 270px;
+  height: 40px;
+  margin: 3px;
+  padding: 0px;
+  display: block;
+`;
+export const MyInput = styled.input`
+width: 300px;
+height: 37px;
+margin-left: 21px;
+align-items: center;
+
+:focus {
+  padding : 13px;
+  border: 0.5px solid #8a8a8a;
+  border-radius: 3px;
+  width: 0px auto;
+  outline: none;
+}
+  ::placeholder {
+    width: 300px;
+    font-size: 15px;
+    
+    color: #8e8e8e;    
+  }
+`;
+
+export const MyLabel = styled.label`
+  margin-right: 40px auto;
+  height: 30px;
+`;
+export const SubmitInput = styled.input`
+  background-color: #8873F0;
+  border: none;
+  color: white;
+  width: 300px;
+  height: 45px;
+  text-decoration: none;
+  text-align: center;
+  font-size: 12px;
+  cursor: pointer;
+`;
+
+
 
 export default Login;
