cfe3f1c80c645b00c6359e5d6db494a8685e4f44
hyooyh8910 <hyooyh8910@naver.com>
2022-06-12 23:43:07 +0900

    (view)Card

--- src/pages/Main.js
+++ src/pages/Main.js
@@ -1,5 +1,27 @@
+import React from "react";
+
+import Card from "../elements/Card";
+import Header from "../components/Header";
+import Header2 from "../components/Header2"
+import Grid from '@mui/material/Grid';
+
 const Main = () => {
-  return <div>Main 페이지입니다</div>;
+
+  return(
+    <>
+    <Header />
+    <Header2 />
+    <Grid container spacing={2}>
+    <Card style={{
+      width: '70vw'}}/>
+      <Card/>
+      <Card/>
+      <Card/>
+    </Grid>
+    </>
+  )
+
+  
 };
 
 export default Main;
